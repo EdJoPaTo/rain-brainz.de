e19900e772eee7e79465a963354185ebc6806c6e
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-12-31 15:45:04 +0100

    chore(toServer): rsync --delay-updates

--- toServer.sh
+++ toServer.sh
@@ -3,4 +3,4 @@ set -e
 
 rm -rf public/generated/
 ./generate.sh
-rsync -acv --compress --omit-dir-times --copy-links --exclude=.DS_Store --delete-delay public/ xmas2014.3t0.de:/var/www/rain-brainz.de/
+rsync -acv --compress --omit-dir-times --copy-links --exclude=.DS_Store --delete-delay --delay-updates public/ xmas2014.3t0.de:/var/www/rain-brainz.de/
