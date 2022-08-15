1b4a3bf2118c97b2ed8546dc8acad827d6027d93
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2022-08-15 18:56:57 +0200

    chore: improve rsync command

--- toServer.sh
+++ toServer.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-set -e
+set -eu
 
 rm -rf public/generated/
 ./generate.sh
 rsync \
-    --recursive --perms --times \
+    --recursive --perms --times --omit-dir-times \
     --compress --verbose --checksum --delete-delay --delay-updates \
     --exclude=.DS_Store \
     public/ xmas2014.3t0.de:/var/www/rain-brainz.de/
