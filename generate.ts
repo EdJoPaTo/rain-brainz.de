cfbcb17cb16a01753b2218674d977a2fd6b77a77
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-01-29 15:27:14 +0100

    build: migrate to Deno and order by EXIF:DateTime

---
+++ generate.ts
@@ -0,0 +1,128 @@
+import * as imagemagick from "./imagemagick.ts";
+
+function fileExists(path: string) {
+  try {
+    return Deno.statSync(path).isFile;
+  } catch {
+    return false;
+  }
+}
+
+const originals: imagemagick.ImageMeta[] = [];
+
+for await (const e of Deno.readDirSync("originals")) {
+  if (!e.isFile) continue;
+  if (e.name === ".gitkeep") continue;
+  const { name } = e;
+  console.log("identify", originals.length, name);
+
+  const meta = await imagemagick.identify("originals/" + name);
+  originals.push(meta);
+}
+
+console.log("total originals found", originals.length);
+
+originals.sort((a, b) => a.name.localeCompare(b.name));
+originals.sort((a, b) => a.datetime.localeCompare(b.datetime));
+
+await Deno.mkdir("public/i/", { recursive: true });
+let indexHtml = await Deno.readTextFile("parts/prefix.html");
+
+for (const [index, original] of Object.entries(originals)) {
+  const i = Number(index);
+  console.log(
+    "handle",
+    original.datetime,
+    (i / originals.length).toFixed(2).slice(2) + "%",
+    original.name,
+    original.width,
+    original.height,
+  );
+
+  const thumb = `i/${i}_thumb`;
+  const big = `i/${i}_big`;
+  const download = `i/${i}_download`;
+
+  const thumbResize = [
+    "-resize",
+    "450x300^",
+    "-gravity",
+    "Center",
+    "-extent",
+    "450x300",
+  ];
+  const jpgQuality = ["-sampling-factor", "4:2:0", "-quality"];
+
+  if (!fileExists(`public/${big}.avif`)) {
+    await Promise.all([
+      imagemagick.convert(
+        original.name,
+        "-strip",
+        "-resize",
+        "3000x2500>",
+        `public/${big}.avif`,
+      ),
+      imagemagick.convert(
+        original.name,
+        "-strip",
+        ...thumbResize,
+        `public/${thumb}.avif`,
+      ),
+      imagemagick.convert(
+        original.name,
+        "-strip",
+        ...jpgQuality,
+        "95",
+        `public/${download}.jpg`,
+      ),
+      imagemagick.convert(
+        original.name,
+        "-strip",
+        "-resize",
+        "2000x1500>",
+        ...jpgQuality,
+        "85",
+        `public/${big}.jpg`,
+      ),
+      imagemagick.convert(
+        original.name,
+        "-strip",
+        ...thumbResize,
+        ...jpgQuality,
+        "85",
+        `public/${thumb}.jpg`,
+      ),
+    ]);
+  }
+
+  indexHtml += `<a href="#${i}">
+\t<picture>
+\t\t<source srcset="${thumb}.avif" type="image/avif">
+\t\t<img src="${thumb}.jpg" width="450" height="300" loading="lazy" alt="Thumbnail of an untitled image" />
+\t</picture>
+</a>
+<div id="${i}" class="lightbox">
+\t<div class="image" style="background-image: url(${big}.jpg); background-image: image-set(url(${big}.avif) type('image/avif'));"></div>
+`;
+
+  if (i > 0) {
+    const before = i - 1;
+    indexHtml +=
+      `\t<a class="before" href="#${before}" aria-label="Go to the image before"></a>\n`;
+  }
+
+  if (i + 1 < originals.length) {
+    const after = i + 1;
+    indexHtml +=
+      `\t<a class="after" href="#${after}" aria-label="Go to the image after"></a>\n`;
+  }
+
+  indexHtml +=
+    `\t<a class="close" href="#_" aria-label="Close the image overlay"></a>
+\t<a class="download" href="${download}.jpg" aria-label="Download the image" download="rain-brainz-${i}"></a>
+</div>
+`;
+}
+
+indexHtml += await Deno.readTextFile("parts/suffix.html");
+await Deno.writeTextFile("public/index.html", indexHtml);
---
+++ imagemagick.ts
@@ -0,0 +1,50 @@
+export type ImageMeta = {
+  readonly name: string;
+  readonly width: number;
+  readonly height: number;
+  readonly datetime: string;
+  readonly model: string;
+  readonly caption: string;
+};
+
+export async function identify(name: string): Promise<ImageMeta> {
+  const output = await exec(
+    "identify",
+    "-format",
+    "%w\n%h\n%[EXIF:DateTime]\n%[EXIF:Model]\n%[EXIF:ImageDescription]\n",
+    name,
+  );
+
+  const match = /^(\d+)\n(\d+)\n(.*)\n(.*)\n([\w\W]*)\n$/.exec(output);
+  if (!match) {
+    throw new Error("failed to parse identify stdout");
+  }
+
+  const width = Number(match[1]);
+  const height = Number(match[2]);
+  const datetime = match[3]!.trim().replace(
+    /(\d{4}).(\d{2}).(\d{2}).(\d{2}).(\d{2}).(\d{2})/,
+    "$1-$2-$3T$4-$5-$6",
+  );
+  const model = match[4]!.trim();
+  const caption = match[5]!.trim();
+
+  return { name, width, height, datetime, model, caption };
+}
+
+export async function convert(...options: string[]): Promise<void> {
+  await exec("nice", "convert", ...options);
+}
+
+async function exec(...cmd: [string, ...string[]]): Promise<string> {
+  const process = Deno.run({ cmd, stdout: "piped", stderr: "null" });
+
+  const status = await process.status();
+  if (!status.success) {
+    throw new Error(`Command ${cmd[0]} was not successful`);
+  }
+
+  const outputBuffer = await process.output();
+  const output = new TextDecoder().decode(outputBuffer);
+  return output;
+}
