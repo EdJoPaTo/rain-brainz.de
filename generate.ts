85fd7793f9fd1fb43c48f33a672a6c54303e7d3a
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-02-05 22:54:39 +0100

    style: use tabs

--- generate.ts
+++ generate.ts
@@ -1,31 +1,31 @@
 import * as imagemagick from "./imagemagick.ts";
 
 function fileExists(path: string) {
-  try {
-    return Deno.statSync(path).isFile;
-  } catch {
-    return false;
-  }
+	try {
+		return Deno.statSync(path).isFile;
+	} catch {
+		return false;
+	}
 }
 
 const originalFiles = [...Deno.readDirSync("originals")]
-  .filter((o) => o.isFile)
-  .map((o) => o.name)
-  .filter((o) => o !== ".gitkeep")
-  .filter((o) => o !== ".DS_Store")
-  .map((name, i) => ({ i, name }));
+	.filter((o) => o.isFile)
+	.map((o) => o.name)
+	.filter((o) => o !== ".gitkeep")
+	.filter((o) => o !== ".DS_Store")
+	.map((name, i) => ({ i, name }));
 
 const originals: imagemagick.ImageMeta[] = [];
 
 for (const { i, name } of originalFiles) {
-  console.log(
-    "identify",
-    (i / originalFiles.length).toFixed(2).slice(2) + "%",
-    originals.length + 1,
-    name,
-  );
-  const meta = await imagemagick.identify("originals/" + name);
-  originals.push(meta);
+	console.log(
+		"identify",
+		(i / originalFiles.length).toFixed(2).slice(2) + "%",
+		originals.length + 1,
+		name,
+	);
+	const meta = await imagemagick.identify("originals/" + name);
+	originals.push(meta);
 }
 
 console.log("total originals found", originals.length);
@@ -37,58 +37,58 @@ await Deno.mkdir("public/i/", { recursive: true });
 let indexHtml = await Deno.readTextFile("parts/prefix.html");
 
 for (const [index, original] of Object.entries(originals)) {
-  const i = Number(index);
-  console.log(
-    "handle",
-    original.datetime,
-    (i / originals.length).toFixed(2).slice(2) + "%",
-    original.name,
-  );
+	const i = Number(index);
+	console.log(
+		"handle",
+		original.datetime,
+		(i / originals.length).toFixed(2).slice(2) + "%",
+		original.name,
+	);
 
-  const thumb = `i/${original.datetime}t`;
-  const big = `i/${original.datetime}b`;
-  const download = `i/${original.datetime}d`;
+	const thumb = `i/${original.datetime}t`;
+	const big = `i/${original.datetime}b`;
+	const download = `i/${original.datetime}d`;
 
-  const jpgSampling = ["-sampling-factor", "4:2:0"];
+	const jpgSampling = ["-sampling-factor", "4:2:0"];
 
-  if (!fileExists(`public/${big}.jpg`)) {
-    await Promise.all([
-      imagemagick.convert(
-        original.name,
-        "-strip",
-        ...jpgSampling,
-        "-quality",
-        "95",
-        `public/${download}.jpg`,
-      ),
-      imagemagick.convert(
-        original.name,
-        "-strip",
-        "-resize",
-        "2000x2000>",
-        ...jpgSampling,
-        "-quality",
-        "85",
-        `public/${big}.jpg`,
-      ),
-      imagemagick.convert(
-        original.name,
-        "-strip",
-        "-resize",
-        "450x400>",
-        ...jpgSampling,
-        "-quality",
-        "85",
-        `public/${thumb}.jpg`,
-      ),
-    ]);
-  }
+	if (!fileExists(`public/${big}.jpg`)) {
+		await Promise.all([
+			imagemagick.convert(
+				original.name,
+				"-strip",
+				...jpgSampling,
+				"-quality",
+				"95",
+				`public/${download}.jpg`,
+			),
+			imagemagick.convert(
+				original.name,
+				"-strip",
+				"-resize",
+				"2000x2000>",
+				...jpgSampling,
+				"-quality",
+				"85",
+				`public/${big}.jpg`,
+			),
+			imagemagick.convert(
+				original.name,
+				"-strip",
+				"-resize",
+				"450x400>",
+				...jpgSampling,
+				"-quality",
+				"85",
+				`public/${thumb}.jpg`,
+			),
+		]);
+	}
 
-  const caption = original.caption || "untitled image";
-  const { width: thumbWidth, height: thumbHeight } = await imagemagick
-    .identifySize(`public/${thumb}.jpg`);
+	const caption = original.caption || "untitled image";
+	const { width: thumbWidth, height: thumbHeight } = await imagemagick
+		.identifySize(`public/${thumb}.jpg`);
 
-  indexHtml += `<div class="thumbbox">
+	indexHtml += `<div class="thumbbox">
 \t<a href="#${original.datetime}">
 \t\t<img src="${thumb}.jpg" width="${thumbWidth}" height="${thumbHeight}" loading="lazy" alt="${caption}" />
 \t</a>
@@ -97,20 +97,20 @@ for (const [index, original] of Object.entries(originals)) {
 \t<div class="image" style="background-image: url(${big}.jpg);"></div>
 `;
 
-  if (i > 0) {
-    const before = originals[i - 1]!.datetime;
-    indexHtml +=
-      `\t<a class="before" href="#${before}" aria-label="Go to the image before"></a>\n`;
-  }
+	if (i > 0) {
+		const before = originals[i - 1]!.datetime;
+		indexHtml +=
+			`\t<a class="before" href="#${before}" aria-label="Go to the image before"></a>\n`;
+	}
 
-  if (i + 1 < originals.length) {
-    const after = originals[i + 1]!.datetime;
-    indexHtml +=
-      `\t<a class="after" href="#${after}" aria-label="Go to the image after"></a>\n`;
-  }
+	if (i + 1 < originals.length) {
+		const after = originals[i + 1]!.datetime;
+		indexHtml +=
+			`\t<a class="after" href="#${after}" aria-label="Go to the image after"></a>\n`;
+	}
 
-  indexHtml +=
-    `\t<a class="close" href="#_" aria-label="Close the image overlay"></a>
+	indexHtml +=
+		`\t<a class="close" href="#_" aria-label="Close the image overlay"></a>
 \t<a class="download" href="${download}.jpg" aria-label="Download the image" download="rain-brainz-${original.datetime}"></a>
 </div>
 `;
--- imagemagick.ts
+++ imagemagick.ts
@@ -1,62 +1,62 @@
 export type ImageMeta = {
-  readonly name: string;
-  readonly datetime: string;
-  readonly model: string;
-  readonly caption: string;
+	readonly name: string;
+	readonly datetime: string;
+	readonly model: string;
+	readonly caption: string;
 };
 
 export type Size = {
-  readonly width: number;
-  readonly height: number;
+	readonly width: number;
+	readonly height: number;
 };
 
 export async function identify(name: string): Promise<ImageMeta> {
-  const output = await exec(
-    "identify",
-    "-format",
-    "%[EXIF:DateTime]\n%[EXIF:Model]\n%[EXIF:ImageDescription]\n",
-    name,
-  );
+	const output = await exec(
+		"identify",
+		"-format",
+		"%[EXIF:DateTime]\n%[EXIF:Model]\n%[EXIF:ImageDescription]\n",
+		name,
+	);
 
-  const match = /^(.*)\n(.*)\n([\w\W]*)\n$/.exec(output);
-  if (!match) {
-    throw new Error("failed to parse identify stdout");
-  }
+	const match = /^(.*)\n(.*)\n([\w\W]*)\n$/.exec(output);
+	if (!match) {
+		throw new Error("failed to parse identify stdout");
+	}
 
-  const datetime = match[1]!.trim().replace(
-    /(\d{4}).(\d{2}).(\d{2}).(\d{2}).(\d{2}).(\d{2})/,
-    "$1-$2-$3T$4-$5-$6",
-  );
-  const model = match[2]!.trim();
-  const caption = match[3]!.trim();
-  return { name, datetime, model, caption };
+	const datetime = match[1]!.trim().replace(
+		/(\d{4}).(\d{2}).(\d{2}).(\d{2}).(\d{2}).(\d{2})/,
+		"$1-$2-$3T$4-$5-$6",
+	);
+	const model = match[2]!.trim();
+	const caption = match[3]!.trim();
+	return { name, datetime, model, caption };
 }
 
 export async function identifySize(path: string): Promise<Size> {
-  const output = await exec("identify", path);
+	const output = await exec("identify", path);
 
-  const match = / (\d+)x(\d+) /.exec(output);
-  if (!match) {
-    throw new Error("failed to parse identify stdout");
-  }
+	const match = / (\d+)x(\d+) /.exec(output);
+	if (!match) {
+		throw new Error("failed to parse identify stdout");
+	}
 
-  const width = Number(match[1]);
-  const height = Number(match[2]);
-  return { width, height };
+	const width = Number(match[1]);
+	const height = Number(match[2]);
+	return { width, height };
 }
 
 export async function convert(...options: string[]): Promise<void> {
-  await exec("nice", "convert", ...options);
+	await exec("nice", "convert", ...options);
 }
 
 export async function exec(cmd: string, ...args: string[]): Promise<string> {
-  const process = new Deno.Command(cmd, { args, stdout: "piped" }).spawn();
-  const status = await process.status;
-  if (!status.success) {
-    throw new Error(`Command ${cmd} was not successful`);
-  }
+	const process = new Deno.Command(cmd, { args, stdout: "piped" }).spawn();
+	const status = await process.status;
+	if (!status.success) {
+		throw new Error(`Command ${cmd} was not successful`);
+	}
 
-  const { stdout: outputBuffer } = await process.output();
-  const output = new TextDecoder().decode(outputBuffer);
-  return output;
+	const { stdout: outputBuffer } = await process.output();
+	const output = new TextDecoder().decode(outputBuffer);
+	return output;
 }
